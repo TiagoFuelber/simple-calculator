26a42a11c5d11dccce21112c0e71b6f86c9d7df3
Tiago Fuelber <tiagofuelber@MB-003933.local>
2021-02-11 17:39:29 -0300

    feat(tests): Add some test cases

--- application/eval-expression.js
+++ application/eval-expression.js
@@ -5,7 +5,7 @@ const OPERAND_REGEX = /^-?\d+(\.\d+)?/;
 const NON_SIGNED_OPERAND_REGEX = /^\d+(\.\d+)?/;
 const SUM_EXPRESSION_REGEX = /\+|\-/;
 
-function getTokens(string) {
+export function getTokens(string) {
 	let tokens = [];
 	
 	while (string.length) {
@@ -104,7 +104,7 @@ const OPERATIONS_WHEN_CLEAR_DISPLAY = /\+|\*|\//;
 const LAST_DIGIT_IS_OPERATOR = /(\+|\*|\-|\/)$/;
 const ZERO_WHEN_CLEAR_DISPLAY = /0/;
 
-function processInput(display, input, lastOperationIsCalculation) {
+export function processInput(display, input, lastOperationIsCalculation) {
 	if ((OPERATIONS_WHEN_CLEAR_DISPLAY.test(input) || input === '0') && display === '') {
 		return display;
 	} else if (
@@ -128,158 +128,10 @@ function processInput(display, input, lastOperationIsCalculation) {
 	return display + input;
 }
 
-function validateEqual(input) {
+export function validateEqual(input) {
 	if (/\d$/.test(input)) {
 		return true;
 	}
 
 	return false;
 }
-
-// let input;
-
-// input = processInput('', '*');
-// console.log('should not permit operations when display is clear: ', input, input === '');
-
-// input = processInput('', '/');
-// console.log('should not permit operations when display is clear: ', input, input === '');
-
-// input = processInput('', '+');
-// console.log('should not permit operations when display is clear: ', input, input === '');
-
-// input = processInput('', '-');
-// console.log('should permit minus when display is clear: ', input, input === '-');
-
-// input = validateEqual('1+3+');
-// console.log('should not permit equal: ', input, input === false);
-
-// input = validateEqual('1+3+5');
-// console.log('should permit equal: ', input, input === true);
-
-// input = processInput('1+', '+')
-// console.log('should replace operand: ', input, input === '1+');
-// input = processInput('1+', '*')
-// console.log('should replace operand: ', input, input === '1*');
-// input = processInput('1+', '/')
-// console.log('should replace operand: ', input, input === '1/');
-// input = processInput('1+', '-')
-// console.log('should replace operand: ', input, input === '1-');
-// input = processInput('1-', '+')
-// console.log('should replace operand: ', input, input === '1+');
-// input = processInput('1-', '/')
-// console.log('should replace operand: ', input, input === '1/');
-// input = processInput('1-', '-')
-// console.log('should replace operand: ', input, input === '1-');
-// input = processInput('6/', '-')
-// console.log('should permit minus after operator: ', input, input === '6/-');
-// input = processInput('6*', '-')
-// console.log('should permit minus after operator: ', input, input === '6*-');
-
-// input = processInput('6*-', '*')
-// console.log('should not permit operator after minus: ', input, input === '6**');
-
-// input = processInput('6*-', '+')
-// console.log('should not permit operator after minus: ', input, input === '6*+');
-
-// input = processInput('', '0')
-// console.log('no leading zeros ', input, input === '');
-
-// input = processInput('6/', '0')
-// console.log('no leading zeros ', input, input === '6/');
-
-// input = processInput('2+3*', '0')
-// console.log('no leading zeros ', input, input === '2+3*');
-
-// input = processInput('2+3*1', '0')
-// console.log('no leading zeros ', input, input === '2+3*10');
-
-// input = processInput('-220', '6', true);
-// console.log('after calculation ', input, input === '6');
-
-// input = processInput('6', '8', true);
-// console.log('after calculation ', input, input === '8');
-
-// input = processInput('6', '+', true);
-// console.log('after calculation ', input, input === '6+');
-
-// input = processInput('6', '*', true);
-// console.log('after calculation ', input, input === '6*');
-
-// input = processInput('6', '7', false);
-// console.log('after calculation ', input, input === '67');
-
-// input = processInput('6', '/', false);
-// console.log('after calculation ', input, input === '6/');
-
-// input = processInput('6', '0', false);
-// console.log('after calculation ', input, input === '60');
-
-// input = evalExpression('-8-9*-6');
-// console.log('should keep operations hierarquy', input, input === '46');
-
-// input = getTokens('-8-9*-6');
-// console.log('should tokenize correctly', input, input.toString() === '-8,-,9,*,-6');
-
-// console.warn('========== getTokens ==========');
-// var tokens = getTokens('1+2-4');
-// console.log('should return ["1", "+", "2", "-4"]', tokens, tokens.toString() == '1,+,2,-4');
-
-// tokens = getTokens('-12/5');
-// console.log('should return ["-12", "/", "5"]', tokens, tokens.toString() == '-12,/,5');
-
-// console.warn('========== evalExpression ==========');
-// var result;
-// result = evalExpression('1+2-4');
-// console.log('should return -1: ', result, result === '-1');
-
-// result = evalExpression('8/2');
-// console.log('should return 4: ', result, result === '4');
-
-// result = evalExpression('10/2');
-// console.log('should return 5: ', result, result === '5');
-
-// result = evalExpression('10/2*10');
-// console.log('should return 50: ', result, result === '50');
-
-// result = evalExpression('2+10/2*10');
-// console.log('should return 52: ', result, result === '52');
-
-// result = evalExpression('-0*-0');
-// console.log('should return 0: ', result, result === '0');
-
-// result = evalExpression('3-5');
-// console.log('should return -2: ', result, result === '-2');
-
-// result = evalExpression('-12/-5');
-// console.log('should return 2: ', result, result === '2');
-
-// result = evalExpression('3/3');
-// console.log('should return 1: ', result, result === '1');
-
-// result = evalExpression('213-200+10');
-// console.log('should return 23: ', result, result === '23');
-
-// result = evalExpression('3/5');
-// console.log('should return 0: ', result, result === '0');
-
-// result = evalExpression('63/15');
-// console.log('should return 4: ', result, result === '4');
-
-// result = evalExpression('-12/5');
-// console.log('should return -3: ', result, result === '-3');
-
-// result = evalExpression('3/-0');
-// console.log('should return "": ', result, result == '');
-
-// result = evalExpression('3/0');
-// console.log('should return "": ', result, result == '');
-
-// result = evalExpression('-8-7216/-3*-24*5+9900');
-// console.log('should return -278748', result, result === '-278748');
-
-// result = evalExpression('-0-0-0');
-// console.log('should return 0', result, result === '0');
-
-// console.log(evalExpression(solveMultiplierAndDivisors('2+10/2*10')));
-// console.log(solveMultiplierAndDivisors('2+10/2*10'));
-// console.log(evalExpression('10/2*10'));
--- application/eval-expression.spec.js
+++ application/eval-expression.spec.js
@@ -1,9 +1,226 @@
 const expect = require('chai').expect;
-import { evalExpression } from './eval-expression';
+import {
+    evalExpression, 
+    processInput,
+    validateEqual,
+    getTokens
+} from './eval-expression';
 
 describe('evalExpression', function() {
     it('Should eval correctly string expressions', function() {
-        // expect(evalExpression('2+10/2*10').to.equal('52'));
         expect(evalExpression('2+10/2*10')).to.equal('52');
+    });
+
+    it('should keep operations hierarquy', function() {
+        expect(evalExpression('-8-9*-6')).to.equal('46');
+    });
+
+    it('should return -1', function() {
+        expect(evalExpression('1+2-4')).to.equal('-1');
+    })
+
+    it('should return 4', function() {
+        expect(evalExpression('8/2')).to.equal('4');
+    });
+
+    it('should return 5', function() {
+        expect(evalExpression('10/2')).to.equal('5');
+    });
+
+    it('should return 50', function() {
+        expect(evalExpression('10/2*10')).to.equal('50');
+    });
+
+    it('should return 52', function() {
+        expect(evalExpression('2+10/2*10')).to.equal('52');
+    });
+
+    it('should return 0', function() {
+        expect(evalExpression('-0*-0')).to.equal('0');
+    });
+
+    it('should return -2', function() {
+        expect(evalExpression('3-5')).to.equal('-2');
+    });
+
+    it('should return 2', function() {
+        expect(evalExpression('-12/-5')).to.equal('2');
+    });
+
+    it('should return 1', function() {
+        expect(evalExpression('3/3')).to.equal('1');
+    });
+
+    it('should return 23', function() {
+        expect(evalExpression('213-200+10')).to.equal('23');
+    });
+
+    it('should return 0', function() {
+        expect(evalExpression('3/5').to.equal('0'));
+    });
+
+    it('should return 4', function() {
+        expect(evalExpression('63/15')).to.equal('4');
+    });
+
+    it('should return -3', function() {
+        expect(evalExpression('-12/5')).to.equal('-3');
+    });
+
+    it('should return ""', function() {
+        expect(evalExpression('3/-0')).to.equal('');
+    });
+
+    it('should return ""', function() {
+        expect(evalExpression('3/0')).to.equal('');
+    });
+
+    it('should return -278748', function() {
+        expect(evalExpression('-8-7216/-3*-24*5+9900')).to.equal('-278748');
+    });
+
+    it('should return 0', function() {
+        expect(evalExpression('-0-0-0')).to.equal('0');
+    });
+});
+
+describe('processInput', function() {
+    describe('should handle input correctly when display is clear', function() {
+        it('should not permit operations when display is clear', function() {
+            expect(processInput('', '*')).to.equal('');
+        });
+    
+        it('should not permit operations when display is clear', function() {
+            expect(processInput('', '/')).to.equal('');
+        });
+    
+        it('should not permit operations when display is clear', function() {
+            expect(processInput('', '+')).to.equal('');
+        });
+        
+        it('should permit minus when display is clear', function() {
+            expect(processInput('', '-')).to.equal('-');
+        });
+    });
+
+    describe('should replace operator correctly', function() {
+        it('should replace operand 1+ for 1+', function() {
+            expect(processInput('1+', '+')).to.equal('1+');
+        });
+    
+        it('should replace operand 1+ for 1*', function() {
+            expect(processInput('1+', '*')).to.equal('1*');
+        });
+        
+        it('should replace operand 1+ for 1/', function() {
+            expect(processInput('1+', '/')).to.equal('1/');
+        });
+    
+        it('should replace operand 1+ for 1-', function() {
+            expect(processInput('1+', '-')).to.equal('1-');
+        });
+    
+        it('should replace operand 1- for 1+', function() {
+            expect(processInput('1-', '+')).to.equal('1+');
+        });
+    
+        it('should replace operand 1- for 1/', function() { 
+            expect(processInput('1-', '/')).to.equal('1/');
+        });
+    
+        it('should replace operand 1- for 1-', function() {
+            expect(processInput('1-', '-')).to.equal('1-');
+        });
+    });
+
+    describe('should handle correctly a minus sign before and after another operator', function() {
+        it('should permit minus after / operator', function() {
+            expect(processInput('6/', '-')).to.equal('6/-');
+        });
+
+        it('should permit minus after * operator', function() {
+            expect(processInput('6*', '-')).to.equal('6*-');
+        });
+
+        it('should not permit operator after a minus sign', function() {
+            expect(processInput('6*-', '*')).to.equal('6*');
+        });
+
+        it('should not permit operator after a minus sign', function() {
+            expect(processInput('6*-', '+')).to.equal('6*');
+        });
+    });
+
+    describe('should not permit leading zeros', function() {
+        it('no leading zeros', function() {
+            expect(processInput('', '0')).to.equal('');
+        });
+
+        it('no leading zeros', function() {
+            expect(processInput('6/', '0')).to.equal('6/');
+        });
+
+        it('no leading zeros', function() {
+            expect(processInput('2+3*', '0')).to.equal('2+3*');
+        });
+
+        it('no leading zeros', function() {
+            expect(processInput('2+3*1', '0')).to.equal('2+3*10');
+        });
+    });
+
+    describe('right after a calculation, should start a new one when input is number', function() {
+        it('input number after a calculation', function() {
+            expect(processInput('-220', '6', true)).to.equal('6');
+        });
+
+        it('input number after a calculation', function() {
+            expect(processInput('6', '8', true)).to.equal('8');
+        });
+
+        it('resume the calculation when input is operator', function() {
+            expect(processInput('6', '+', true)).to.equal('6+');
+        });
+        
+        it('resume the calculation when input is operator', function() {
+            expect(processInput('6', '*', true)).to.equal('6*');
+        });
+
+        it('behaves normally when last parameter is false', function() {
+            expect(processInput('6', '7', false)).to.equal('67');
+        });
+
+        it('behaves normally when last parameter is false', function() {
+            expect(processInput('6', '/', false)).to.equal('6/');
+        });
+
+        it('behaves normally when last parameter is false', function() {
+            expect(processInput('6', '0', false)).to.equal('60');
+        });
+    });
+}); 
+
+
+describe('validateEqual', function() {
+    it('should not permit equal when last character is operator', function() {
+        expect(validateEqual('1+3+')).to.equal(false);
+    });
+
+    it('should permit equal when last character is number', function() {
+        expect(validateEqual('1+3+5')).to.equal(true);
+    });
+});
+
+describe('getTokens', function() {
+    it('should tokenize correctly', function() {
+        expect(getTokens('-8-9*-6')).to.equal(['-8', '-', '9', '*', '-6']);
+    });
+
+    it('should tokenize correctly', function() {
+        expect(getTokens('1+2-4')).to.equal(['1', '+', '2', '-', '4']);
+    });
+
+    it('should tokenize correctly', function() {
+        expect(getTokens('-12/5')).to.equal(['-12', '/', '5']);
     });
 });
